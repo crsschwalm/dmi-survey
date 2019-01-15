3ed895ed31b372a516ef05443bd6411071ef78e4
Carson Schwalm <crsschwalm@gmail.com>
2019-01-15 17:40:12 -0500

    update cors headers (#10)

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const path = require('path');
 let app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
-const port = process.env.PORT || "8080";
-app.set("port", port);
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
+app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || '8080';
+app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
